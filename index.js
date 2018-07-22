6b118ece9906a73429a74b7f979f63e2dd831254
Matt Tilley <mtilley255@gmail.com>
2018-07-22 11:10:57 -0500

    Getting back 'undefined' now, but no error at least.

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-let x = $(document).ready(function(){
+let tempVal;
+
+
+$(document).ready(function(){
     
     $("#seedType").on("change", function() {
         
-        let tempVal = $(this).val();
+        return tempVal = $(this).val();
         
-        return tempVal;
     })
     
-   console.log(x());
-    
-    // Now I put in the parens to run this like a function, but it's not
-    // working.
-    
+   
+   // Now I'm not getting an error, but rather "undefined" instead.
     
    
 });
