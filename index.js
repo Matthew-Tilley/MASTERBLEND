aaa10144794276ddd629689a7b567346a166e221
Matt Tilley <mtilley255@gmail.com>
2018-07-22 11:03:32 -0500

    Created function / works just fine - as expected.

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 $(document).ready(function(){
     
-
+    $("#seedType").on("change", function() {
+        
+        let tempVal = $(this).val();
+        
+        console.log(tempVal)
+    })
+    
+   // THIS WORKS FINE
+    
+    
    
 });
 
 
- function typeGet() {
-    
-        console.log("BLURP");
-     
-     $("body").css("background-color", "blue");
-        
-    }
-
-
-    
