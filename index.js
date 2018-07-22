0e6f260eaa7a92717696811665300827da551e53
Matt Tilley <mtilley255@gmail.com>
2018-07-22 11:14:24 -0500

    Tried creating function / not working at all.

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-let tempVal;
+
 
 
 $(document).ready(function(){
     
-    $("#seedType").on("change", function() {
+ /*   $("#seedType").on("change", function() {
         
         return tempVal = $(this).val();
         
-    })
+    })*/
     
    
    // Now I'm not getting an error, but rather "undefined" instead.
     
    
 });
+
+
+let tempVal = function(){
+    document.getElementById("seedType").val();
+};
+
+console.log(tempVal());
 
 
