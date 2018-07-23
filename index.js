831a798242a23ad9da6f7ecef7daa29de260f2be
Matt Tilley <mtilley255@gmail.com>
2018-07-23 08:31:00 -0500

    Added / refactored another function for mixture #2.

--- index.js
+++ index.js
@@ -36,17 +36,13 @@ let app = new Vue({
             
             if (this.seedType == "NUHG")
                 {
-                    this.masterBlend =      this.gallons * 2.4;
-                    this.calciumNitrate =   this.gallons * 2.4;
-                    this.magnesiumSulfate = this.gallons * 1.2;
+                    this.mixture_2();
                 }
             
             
             if (this.seedType == "PEPR")
                 {
-                    this.masterBlend =      this.gallons * 2;
-                    this.calciumNitrate =   this.gallons * 2;
-                    this.magnesiumSulfate = this.gallons * 1;
+                    this.mixture_2();
                 }
             
             
@@ -56,11 +52,32 @@ let app = new Vue({
         
         mixture_1: function() {
             
-            this.masterBlend =      this.gallons * 20000;
-            this.calciumNitrate =   this.gallons * 200000;
+            this.masterBlend =      this.gallons * 2;
+            this.calciumNitrate =   this.gallons * 2;
             this.magnesiumSulfate = this.gallons * 1;
             
-        }
+        },  /*  END MIXTURE #1 FUNCTION  */
+        
+        
+        mixture_2: function() {
+            
+            this.masterBlend =      this.gallons * 2.4;
+            this.calciumNitrate =   this.gallons * 2.4;
+            this.magnesiumSulfate = this.gallons * 1.2;
+            
+        },  /*  END MIXTURE #2 FUNCTION  */
+        
+        
+        mixture_3: function() {
+            
+            this.masterBlend =      this.gallons * 999;
+            this.calciumNitrate =   this.gallons * 999;
+            this.magnesiumSulfate = this.gallons * 999;
+            
+        
+        }, /*  END MIXTURE #3 FUNCTION  */
+        
+        
         
         
     }   /*  END METHODS SECTION  */
